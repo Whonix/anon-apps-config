db2a00141c2f316ac525b4d5a585179c7aa5a604
Patrick Schleizer <adrelanos@riseup.net>
2020-04-06 09:25:23 -0400

    import from anon-gpg-conf

---
+++ etc/thunderbird/pref/30_whonix.js
@@ -0,0 +1,8 @@
+//#### meta start
+//#### project Whonix
+//#### category networking and apps
+//#### description https://phabricator.whonix.org/T652
+//#### meta end
+
+// https://phabricator.whonix.org/T652
+pref("extensions.torbirdy.gpg_already_torified", true);
